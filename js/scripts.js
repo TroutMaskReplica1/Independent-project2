f5cdd7feabdd4082e8f5ccca3642e12424a138f1
Ryan Dikeos <ryan.dikeos@gmail.com>
2019-01-18 11:15:59 -0800

    add more forms

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,25 @@
-var ruby = $("#ruby").val();
-
-var php = $("#php").val();
-
-var csharp = $("#csharp").val();
-
-
 $(document).ready(function() {
-$('form#track-form').submit(function(event) {
+$('form#track-form1').submit(function(event) {
+$('form#track-form2').submit(function(event) {
+$('form#track-form3').submit(function(event) {
+$('form#track-form4').submit(function(event) {
+$('form#track-form5').submit(function(event) {
+  // var ruby = parseInt($("#ruby").val());
+  // var php = parseInt($("#php").val());
+  // var csharp = parseInt($("#csharp").val());
+  // var trackInput = $("input#track-input").val();
+  // if (trackInput < 2) {
+  //   $("#ruby").show();
+  // } else if (trackInput > 1) {
+  //   alert("PHP!!!")
+  //   $("#php").show();
+  // } else if (trackInput > 2){
+  //   $("#csharp").show();
+  // }
   event.preventDefault();
-  var trackInput = $("input#track-input").val();
-    if (trackInput = ruby) {
-      $("#ruby").show();
-  } else if (trackInput = php) {
-      alert("PHP!!!")
-      $("#php").show();
-  } else {
-      $("#csharp").show();
-  }
+});
+});
+});
+});
 });
 });

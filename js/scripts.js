b2b233da5954b19074ccd8b21e75f4a7d42349b1
Ryan Dikeos <ryan.dikeos@gmail.com>
2019-01-18 15:29:23 -0800

    add funtionality to js, change branching a tad

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,18 @@
 $(document).ready(function() {
   $("#question").submit(function() {
-  // $("#track-form2").submit(function() {
-  // $("#track-form3").submit(function() {
-  // $("#track-form4").submit(function() {
-  // $("#track-form5").submit(function() {
     var q1 = $("input#track-input1").val();
     var q2 = $("input#track-input2").val();
     var q3 = $("input#track-input3").val();
     var q4 = $("input#track-input4").val();
     var q5 = $("input#track-input5").val();
 
-    if (q1 == "yes") {
-      alert("FER")
-      ("#ruby").show();
-    } else {
-      alert("d");
+    if (q1 == "yes" && q2 == "yes" && q3 == "no" && q4 == "front end" && q5 == "yes") {
+      $("#ruby").show();
+    } else if (q1 == "yes" && q2 == "yes" && q3 == "no" && q4 == "front end" && q5 == "yes") {
+      $("#php").show();
+    } else if (q1 == "yes" && q2 == "yes" && q3 == "no" && q4 == "front end" && q5 == "yes") {
+      $("#csharp").show();
     }
     event.preventDefault();
 });
 });
-// });
-// });
-// });
-// });

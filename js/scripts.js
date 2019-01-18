eead6a96e6e223ccb9ec79e2f7e1f8de02db2399
Ryan Dikeos <ryan.dikeos@gmail.com>
2019-01-18 12:45:22 -0800

    add styling

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,17 @@ $('form#track-form2').submit(function(event) {
 $('form#track-form3').submit(function(event) {
 $('form#track-form4').submit(function(event) {
 $('form#track-form5').submit(function(event) {
-  var q1 = ("input#track-input1").val();
-  var q2 = ("input#track-input2").val();
-  var q3 = ("input#track-input3").val();
-  var q4 = ("input#track-input4").val();
-  var q5 = ("input#track-input5").val();
+  // var ok = document.getElementById("").val();
+  var q1 = $("input#track-input1").val();
+  var q2 = $("input#track-input2").val();
+  var q3 = $("input#track-input3").val();
+  var q4 = $("input#track-input4").val();
+  var q5 = $("input#track-input5").val();
 
-  if (q1 )
+  if (q1 === "yes") {
+    alert("FER")
+    ("#ruby").show();
+  }
   event.preventDefault();
 });
 });
